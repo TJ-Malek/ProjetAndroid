e11ec63170dc44227b021bc388c576b22a060b16
ilyes94 <ilyes.mk94@gmail.com>
2021-06-24 14:43:57 +0200

    Ajout class pilotes/users + CRUD pilotes/users

---
+++ APIs/api_Aerosoft/api/crudUsers/read.php
@@ -0,0 +1,47 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    include_once '../../config/database.php';
+    include_once '../../class/utilisateur.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $items = new Utilisateur($db);
+
+    $stmt = $items->getUtilisateurs();
+    $itemCount = $stmt->rowCount();
+
+
+    // echo json_encode($itemCount);
+    
+    if($itemCount > 0){
+        
+        $UserArr = array();
+        $UserArr["utilisateurs"] = array();
+       
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "IdUtilisateur" => $IdUtilisateur,
+                "Nom" => $Nom,
+                "Prenom" => $Prenom,
+                "Mail" => $Mail,
+                "Statut" => $Statut,
+                "RoleNom" => $RoleNom
+            );
+
+            array_push($UserArr["utilisateurs"], $e);
+        }
+        echo json_encode($UserArr);
+    }
+
+    else{
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "No record found.")
+        );
+    }
+?>
---
+++ APIs/api_Aerosoft/class/utilisateur.php
@@ -0,0 +1,110 @@
+<?php
+    class Utilisateur{
+
+        // Connection
+        private $conn;
+
+        // Table
+        private $db_table = ["utilisateur", "roles"];
+
+        // Columns
+        public $IdUtilisateur;
+        public $Nom;
+        public $Prenom;
+        public $Mail;
+        public $MotDePasse;
+        public $Statut;
+        public $IdRole;
+       
+
+        // Db connection
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        // GET ALL
+        public function getUtilisateurs(){
+            $sqlQuery = "SELECT * FROM " . $this->db_table[0] . "
+                        INNER JOIN " . $this->db_table[1] ."
+                        ON roles.IdRole = utilisateur.IdRole";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        // CREATE
+        public function createUtilisateur(){
+            if($this->VerifUser()==null){
+                echo json_encode("we in");
+                $sqlQuery = "INSERT INTO ". $this->db_table[0] ."
+                        SET 
+                            IdUtilisateur = :IdUtilisateur,
+                            Nom = :Nom, 
+                            Prenom  = :Prenom, 
+                            Mail = :Mail, 
+                            MotDePasse = :MotDePasse,
+                            Statut = :Statut,
+                            IdRole = :IdRole";
+
+
+                $stmt = $this->conn->prepare($sqlQuery);
+
+                // sanitize
+                $this->IdUtilisateur=htmlspecialchars(strip_tags($this->IdUtilisateur));
+                $this->Nom=htmlspecialchars(strip_tags($this->Nom));
+                $this->Prenom=htmlspecialchars(strip_tags($this->Prenom));
+                $this->Mail=htmlspecialchars(strip_tags($this->Mail));
+                $this->MotDePasse=htmlspecialchars(strip_tags($this->MotDePasse));
+                $this->Statut=htmlspecialchars(strip_tags($this->Statut));
+                $this->IdRole=htmlspecialchars(strip_tags($this->IdRole));
+
+                // bind data
+                $stmt->bindParam(":IdUtilisateur", $this->IdUtilisateur);
+                $stmt->bindParam(":Nom", $this->Nom);
+                $stmt->bindParam(":Prenom ", $this->Prenom);
+                $stmt->bindParam(":Mail", $this->Mail);
+                $stmt->bindParam(":MotDePasse", $this->MotDePasse);
+                $stmt->bindParam(":Statut", $this->Statut);
+                $stmt->bindParam(":IdRole", $this->IdRole);
+
+                echo json_encode($this->IdUtilisateur." ". $this->Nom ." ". $this->Prenom ." ". $this->Mail . " ". $this->MotDePasse . " ". $this->Statut." ". $this->IdRole);
+                echo json_encode($stmt);
+                if($stmt->execute()){
+                return true;
+                }
+                    return false;
+            }
+            else {
+                echo "User existe déjà dans la base";
+            }
+        }
+        public function VerifUser(){
+            $sqlQuery = "SELECT
+                         
+            Nom, 
+            Prenom , 
+            Mail, 
+            MotDePasse,
+            Statut,
+            IdRole
+         
+          FROM
+            ". $this->db_table[0] ."
+        WHERE 
+        IdUtilisateur = ?
+        LIMIT 0,1";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+
+        $stmt->bindParam(1, $this->IdUtilisateur);
+
+        $stmt->execute();
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $data; 
+
+        }
+
+    
+
+    }
